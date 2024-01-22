aa08971d13f22807c6a5b68b5336b540e473a3be
Patrick <muilipato@gmail.com>
2024-01-22 17:55:51 +0300

    added sql to create the table

---
+++ lib/song.rb
@@ -0,0 +1,22 @@
+class Song
+  attr_accessor :name, :album, :id
+
+    def initialize name: , album:, id: nil
+      @id = id #Initialize id because of the database
+      @name = name
+      @album = album
+
+    end
+
+    def self.create_table
+      sql = <<-SQL
+         CREATE TABLE IF NOT EXISTS songs (
+          id INTEGER PRIMARY KEY,
+          name TEXT,
+          album TEXT,
+         )
+         SQL
+         DB[:conn].execute(sql)
+    end
+
+end
