f37291e2e93733ec00cfc407a24ba6b141047f46
Patrick <muilipato@gmail.com>
2024-01-22 18:28:10 +0300

    created save method to grab our instance attributes and save them to the database

--- lib/song.rb
+++ lib/song.rb
@@ -8,7 +8,7 @@ class Song
 
     end
 
-    def self.create_table
+    def self.create_table #This just creates the table and no data is inserted
       sql =  <<-SQL
          CREATE TABLE IF NOT EXISTS songs (
           id INTEGER PRIMARY KEY,
@@ -18,5 +18,23 @@ class Song
          SQL
       DB[:conn].execute(sql)
     end
+
+    #To insert any data let's define an instance method
+    #to save the attributes of that instance to the db
+
+    def save
+      sql = <<-SQL
+      INSERT INTO songs (name, album)
+      VALUES (?,?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.album)
+    end
+
+    def self.create name: , album:
+      song = Song.new name: name, album: album
+      song.save
+    end
+
 
 end
